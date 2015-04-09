8d102199487e3e992066dfe5fc0357d6c4764a2b
Jacob Gorney <jtgorney@svsu.edu>
2015-04-09 10:57:13 -0400

    Picking fully functional

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -13,12 +13,18 @@
 (import 'java.awt.Font)
 (import 'javax.swing.JOptionPane)
 
+; Check the game for a winner.
+(defn checkGame []
+
+)
+
 (defn runGame []
 	; Define some variables
 	(def playerScore 0)
 	(def computerScore 0)
 	(def playerX '#{})
 	(def playerO '#{})
+	(def allNums '#{0 1 2 3 4 5 6 7 8})
 
 	; Define the frame
 	(def frame (JFrame. "Tic Tac Toe"))
@@ -43,6 +49,18 @@
 	(def btn_x3y1 (JButton. " "))
 	(def btn_x3y2 (JButton. " "))
 	(def btn_x3y3 (JButton. " "))
+
+	(.setName btn_x1y1 "0")
+	(.setName btn_x1y2 "1")
+	(.setName btn_x1y3 "2")
+
+	(.setName btn_x2y1 "3")
+	(.setName btn_x2y2 "4")
+	(.setName btn_x2y3 "5")
+
+	(.setName btn_x3y1 "6")
+	(.setName btn_x3y2 "7")
+	(.setName btn_x3y3 "8")
 
 	(.setFont btn_x1y1 (Font. "Arial", 1, 50))
 	(.setFont btn_x1y2 (Font. "Arial", 1, 50))
@@ -75,21 +93,14 @@
         	; Player's choice
         	(.setText (.getSource event) "O")
         	(.setEnabled (.getSource event) false)
-
-
-
-
         	; Determine which button was clicked and add to moves list.
-
-
-
-
-
+        	; Get the selected tile and add to the set
+        	(def selected (. Integer parseInt (.getName (.getSource event))))
+        	(def playerO (conj playerO selected))
         	; Perform computer actions now
         	; Generate random number
-        	(def available (seq (set/intersection (set/union playerX playerO) playerX)))
-        	(def totalSpots (- 8 (count available)))
-        	(def tile (int (math/floor (rand totalSpots))))
+        	(def available (seq (set/difference allNums (set/union playerX playerO))))
+        	(def tile (nth available (int (math/floor (rand (- (count available) 1))))))
         	; Redefine playerX with the new selection
         	(def playerX (conj playerX tile))
         	; Now just mark the right button with X
